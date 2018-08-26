8fe5da114d810cdac4bcfd318d8e85232c0071ae
bradnull <brad@bradleynull.com>
2018-08-26 17:44:11 -0500

    Create PyGame GUI for PiTFT
    This adds a class for displaying images to the PiTFT 2.4''
    display. The images can now be seen both through Flask and
    the display!

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import cv2
 import requests
 import signal
 from threading import Thread
+from gui.pitft_main_window import PiTftMainWindow
 
 app = Flask(__name__)
 """The global Flask app used for talking to clients through HTTP"""
@@ -74,9 +75,17 @@ def shutdown_flask_server():
 
 
 if __name__ == '__main__':
+    # The main PyGame window that we will use to display and get touch
+    # screen input from
+    mw = PiTftMainWindow((320, 240))
+
     # Start the app so that anyone on the network can see it
-    vid_proc = VideoProcessor(VideoCamera(device=0, width=1280, height=720,
-                                          multithread=True))
+    vid_proc = VideoProcessor(VideoCamera(device=0,
+                                          width=mw.size[0],
+                                          height=mw.size[1],
+                                          fps=60,
+                                          multithread=False))
+
     # Get one frame so that the video fee will populate
     vid_proc.get_frame()
     # Kick off the viewer in another thread to get it out of the way
@@ -85,13 +94,23 @@ if __name__ == '__main__':
 
     def signal_handler(sig, frame):
         global running
-
-        requests.post("http://127.0.0.1:5000/shutdown")
-        flask_thread.join(timeout=1)
         running = False
+        print("\nPatrolBot: Caught SIGINT. Quitting.")
+        requests.post("http://localhost:5000/shutdown")
+        flask_thread.join(timeout=1)
 
     signal.signal(signal.SIGINT, signal_handler)
     running = True
 
     while running:
         vid_proc.get_frame(show_fps=True)
+        mw.update(
+            cv2.cvtColor(
+                cv2.resize(
+                    cv2.flip(
+                        cv2.rotate(vid_proc.get_last_frame(),
+                                   cv2.ROTATE_90_COUNTERCLOCKWISE),
+                        0),
+                    (mw.size[1], mw.size[0])),
+                cv2.COLOR_BGR2RGB)
+        )
